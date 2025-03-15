f33397772598f55d0bc7d2077c2ec4eb0899f77a
Takuto Yamada <133118353+takyamata@users.noreply.github.com>
2025-03-15 22:32:26 +0900

    swiperをローカルに持ってくるように試みる

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import styles from './home.module.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCards } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-cards';
 
 const Home = () => {
-    return <div>Home画面</div>;
+    return (
+        <div className={styles.root}>
+            <Swiper 
+				effect={'cards'} 
+				grabCursor={true} 
+				modules={[EffectCards]} 
+				className={styles.swiper}
+			>
+                <SwiperSlide className={styles.swiperSlide}>Slide 1</SwiperSlide>
+                <SwiperSlide className={styles.swiperSlide}>Slide 2</SwiperSlide>
+                <SwiperSlide className={styles.swiperSlide}>Slide 3</SwiperSlide>
+                <SwiperSlide className={styles.swiperSlide}>Slide 4</SwiperSlide>
+                <SwiperSlide className={styles.swiperSlide}>Slide 5</SwiperSlide>
+                <SwiperSlide className={styles.swiperSlide}>Slide 6</SwiperSlide>
+                <SwiperSlide className={styles.swiperSlide}>Slide 7</SwiperSlide>
+                <SwiperSlide className={styles.swiperSlide}>Slide 8</SwiperSlide>
+                <SwiperSlide className={styles.swiperSlide}>Slide 9</SwiperSlide>
+            </Swiper>
+        </div>
+    );
 };
 
 export default Home;
--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
+            '@utils': path.resolve(__dirname, 'src/utils'),
             '@assets': path.resolve(__dirname, 'src/assets'),
             '@components': path.resolve(__dirname, 'src/components'),
             '@layout': path.resolve(__dirname, 'src/layout'),
